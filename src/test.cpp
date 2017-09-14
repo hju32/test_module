21390f5352188806e1e0c4764ef22f49d6baca21
Nick <zhangzyhack@126.com>
2017-09-14 17:42:25 -0400

    add comments

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,10 @@
 //
 // Created by Eric Fang on 7/26/17.
-//
+// edited by Nick Zhang
+
+// this test node should read image published to camera/image
+// and detect target in it and lable that and output that image
+// this is done through target_detector with log enabled
 
 #include <ros/ros.h>
 #include <test_module/target_detector.h>
